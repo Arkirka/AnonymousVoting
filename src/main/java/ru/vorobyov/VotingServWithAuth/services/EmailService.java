e1e9d15c847c69194e079b42e182807d846ca118
Arkirka <denis.vorobyov.00@gmail.com>
2023-01-13 17:04:34 +0300

    refactored

--- src/main/java/ru/vorobyov/VotingServWithAuth/services/EmailService.java
+++
@@ -1,6 +0,0 @@
-package ru.vorobyov.VotingServWithAuth.services;
-
-public interface EmailService {
-    public void sendSimpleEmail(String toAddress, String subject, String message);
-    public void sendEmailWithAttachment(String toAddress, String subject, String message, String attachment);
-}
