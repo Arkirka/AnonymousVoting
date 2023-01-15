d57a2e1cd9faef31228cafee64e4193f5a9ae952
Arkirka <denis.vorobyov.00@gmail.com>
2023-01-15 20:29:56 +0300

    all repositories tests added & refactored

--- src/test/java/ru/vorobyov/VotingServWithAuth/repositories/ActivateLinkRepositoryTest.java
+++ src/test/java/ru/vorobyov/VotingServWithAuth/repositories/ActivateLinkRepositoryTest.java
@@ -2,7 +2,6 @@ package ru.vorobyov.VotingServWithAuth.repositories;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -10,19 +9,15 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.TestPropertySource;
 import ru.vorobyov.VotingServWithAuth.entities.ActivateLink;
 import ru.vorobyov.VotingServWithAuth.entities.User;
 
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @TestPropertySource("classpath:application-test.properties")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -30,12 +25,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DataJpaTest
 class ActivateLinkRepositoryTest {
 
-    @Autowired
-    private DataSource dataSource;
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-    @Autowired
-    private EntityManager entityManager;
     @Autowired
     private UserRepository userRepository;
     @Autowired
@@ -46,40 +35,24 @@ class ActivateLinkRepositoryTest {
         activateLinkRepository.deleteAll();
     }
 
-    @Test
-    void injectedComponentsAreNotNull(){
-        assertThat(dataSource).isNotNull();
-        assertThat(jdbcTemplate).isNotNull();
-        assertThat(entityManager).isNotNull();
-        assertThat(activateLinkRepository).isNotNull();
-        assertThat(userRepository).isNotNull();
-    }
-
     @ParameterizedTest
     @MethodSource("shouldFindNoActivateLinkByLinkIfRepositoryIsEmptyCases")
-    public void shouldFindNoActivateLinkByLinkIfRepositoryIsEmpty(String link) {
-        ActivateLink linkByLink = activateLinkRepository.findActivateLinkByLink(link);
-
-        assertThat(linkByLink).isNull();
+    public void shouldFindNoActivateLinkByLinkIfRepositoryIsEmpty(String source) {
+        Assertions.assertFalse(
+                activateLinkRepository.findActivateLinkByLink(source).isPresent()
+        );
     }
 
     public Stream<Arguments> shouldFindNoActivateLinkByLinkIfRepositoryIsEmptyCases() {
-        return Stream.of(
-                Arguments.arguments("first"),
-                Arguments.arguments("second"),
-                Arguments.arguments("123213123"),
-                Arguments.arguments("dsdasdasd213124efsdfs"),
-                Arguments.arguments("sadsadsafty663")
-        );
+        return Util.oneArgumentStringCases();
     }
 
     @ParameterizedTest
     @MethodSource("shouldFindActivateLinkByLinkIfRepositoryNotEmptyCases")
-    public void shouldFindActivateLinkByLink(String link,
-                                             ActivateLink activateLink) {
-        ActivateLink linkByLink = activateLinkRepository.findActivateLinkByLink(link);
+    public void shouldFindActivateLinkByLink(String link, ActivateLink activateLink) {
+        Optional<ActivateLink> linkByLink = activateLinkRepository.findActivateLinkByLink(link);
 
-        Assertions.assertEquals(activateLink, linkByLink, "Entities not equals!");
+        Assertions.assertEquals(activateLink, linkByLink.orElse(null), "Entities not equals!");
     }
 
     public Stream<Arguments> shouldFindActivateLinkByLinkIfRepositoryNotEmptyCases() {
@@ -89,19 +62,13 @@ class ActivateLinkRepositoryTest {
         List<User> users = Util.createUsers(links.length, userRepository);
         List<ActivateLink> activateLinks = IntStream.range(0, links.length)
                 .unordered()
-                .mapToObj(i -> {
-                    ActivateLink temp = new ActivateLink();
-                    temp.setUser(users.get(i));
-                    temp.setLink(links[i]);
-                    return temp;
-                }).collect(Collectors.toList());
+                .mapToObj(i -> new ActivateLink(links[i], users.get(i)))
+                .collect(Collectors.toList());
         activateLinkRepository.saveAll(activateLinks);
         activateLinks.clear();
-        activateLinkRepository.findAll().forEach(activateLinks::add);
+        activateLinks.addAll(activateLinkRepository.findAll());
         return IntStream.range(0, links.length)
-                .mapToObj(i ->
-                        Arguments.arguments(activateLinks.get(i).getLink(), activateLinks.get(i))
-                );
+                .mapToObj(i -> Arguments.arguments(activateLinks.get(i).getLink(), activateLinks.get(i)));
     }
 
 
--- src/test/java/ru/vorobyov/VotingServWithAuth/repositories/RecoveryLinkRepositoryTest.java
+++ src/test/java/ru/vorobyov/VotingServWithAuth/repositories/RecoveryLinkRepositoryTest.java
@@ -1,25 +1,23 @@
 package ru.vorobyov.VotingServWithAuth.repositories;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.TestPropertySource;
 import ru.vorobyov.VotingServWithAuth.entities.RecoveryLink;
 import ru.vorobyov.VotingServWithAuth.entities.User;
 
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @TestPropertySource("classpath:application-test.properties")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -27,12 +25,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DataJpaTest
 class RecoveryLinkRepositoryTest {
 
-    @Autowired
-    private DataSource dataSource;
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-    @Autowired
-    private EntityManager entityManager;
     @Autowired
     private RecoveryLinkRepository recoveryLinkRepository;
     @Autowired
@@ -43,42 +35,25 @@ class RecoveryLinkRepositoryTest {
         recoveryLinkRepository.deleteAll();
     }
 
-    @Test
-    void injectedComponentsAreNotNull(){
-        assertThat(dataSource).isNotNull();
-        assertThat(jdbcTemplate).isNotNull();
-        assertThat(entityManager).isNotNull();
-        assertThat(recoveryLinkRepository).isNotNull();
-    }
-    @Test
-    void findRecoveryLinkByLink() {
-    }
-
     @ParameterizedTest
     @MethodSource("shouldFindNoRecoveryLinkByLinkIfRepositoryIfEmptyCases")
     public void shouldFindNoRecoveryLinkByLinkIfRepositoryIfEmpty(String source) {
-        RecoveryLink linkByLink = recoveryLinkRepository.findRecoveryLinkByLink(source);
-
-        assertThat(linkByLink).isNull();
+        Assertions.assertFalse(
+                recoveryLinkRepository.findRecoveryLinkByLink(source).isPresent()
+        );
     }
 
     public Stream<Arguments> shouldFindNoRecoveryLinkByLinkIfRepositoryIfEmptyCases() {
-        return Stream.of(
-                Arguments.of("first"),
-                Arguments.of("second"),
-                Arguments.of("123213123"),
-                Arguments.of("dsdasdasd213124efsdfs"),
-                Arguments.of("sadsadsafty663")
-        );
+        return Util.oneArgumentStringCases();
     }
 
     @ParameterizedTest
     @MethodSource("shouldFindRecoveryLinkByLinkIfRepositoryNotEmptyCases")
     public void shouldFindRecoveryLinkByLink(String source,
                                              RecoveryLink expected) {
-        RecoveryLink plsWork = recoveryLinkRepository.findRecoveryLinkByLink(source);
+        Optional<RecoveryLink> plsWork = recoveryLinkRepository.findRecoveryLinkByLink(source);
 
-        Assertions.assertEquals(expected, plsWork, "Entities not equals!");
+        Assertions.assertEquals(expected, plsWork.orElse(null), "Entities not equals!");
     }
 
     public Stream<Arguments> shouldFindRecoveryLinkByLinkIfRepositoryNotEmptyCases() {
@@ -88,30 +63,21 @@ class RecoveryLinkRepositoryTest {
         List<User> users = Util.createUsers(links.length, userRepository);
         List<RecoveryLink> recoveryLinks = IntStream.range(0, links.length)
                 .unordered()
-                .mapToObj(i -> {
-                    RecoveryLink temp = new RecoveryLink();
-                    temp.setUser(users.get(i));
-                    temp.setLink(links[i]);
-                    return temp;
-                }).collect(Collectors.toList());
+                .mapToObj(i -> new RecoveryLink(links[i], users.get(i)))
+                .collect(Collectors.toList());
         recoveryLinkRepository.saveAll(recoveryLinks);
         recoveryLinks.clear();
-        recoveryLinkRepository.findAll().forEach(recoveryLinks::add);
+        recoveryLinks.addAll(recoveryLinkRepository.findAll());
         return IntStream.range(0, links.length)
-                .mapToObj(i ->
-                        Arguments.of(recoveryLinks.get(i).getLink(), recoveryLinks.get(i))
-                );
-    }
-
-    @Test
-    void findRecoveryLinksByUser() {
+                .mapToObj(i -> Arguments.of(recoveryLinks.get(i).getLink(), recoveryLinks.get(i)));
     }
 
     @ParameterizedTest
     @MethodSource("shouldNotFindRecoveryLinkByUserIfEmptyCases")
     public void shouldNotFindRecoveryLinkByUserIfEmpty(User source) {
-        RecoveryLink plsWork = recoveryLinkRepository.findRecoveryLinkByUser(source);
-        assertThat(plsWork).isNull();
+        Assertions.assertFalse(
+                recoveryLinkRepository.findRecoveryLinkByUser(source).isPresent()
+        );
     }
 
     public Stream<Arguments> shouldNotFindRecoveryLinkByUserIfEmptyCases() {
@@ -123,9 +89,9 @@ class RecoveryLinkRepositoryTest {
     @MethodSource("shouldFindRecoveryLinkByUserCases")
     public void shouldFindRecoveryLinkByUser(User source,
                                              RecoveryLink expected) {
-        RecoveryLink plsWork = recoveryLinkRepository.findRecoveryLinkByUser(source);
+        Optional<RecoveryLink> plsWork = recoveryLinkRepository.findRecoveryLinkByUser(source);
 
-        Assertions.assertEquals(expected, plsWork, "Entities not equals!");
+        Assertions.assertEquals(expected, plsWork.orElse(null), "Entities not equals!");
     }
 
     public Stream<Arguments> shouldFindRecoveryLinkByUserCases() {
@@ -135,20 +101,12 @@ class RecoveryLinkRepositoryTest {
         List<User> users = Util.createUsers(links.length, userRepository);
         List<RecoveryLink> recoveryLinks = IntStream.range(0, links.length)
                 .unordered()
-                .mapToObj(i -> {
-                    RecoveryLink temp = new RecoveryLink();
-                    temp.setUser(users.get(i));
-                    temp.setLink(links[i]);
-                    return temp;
-                }).collect(Collectors.toList());
+                .mapToObj(i -> new RecoveryLink(links[i], users.get(i)))
+                .collect(Collectors.toList());
         recoveryLinkRepository.saveAll(recoveryLinks);
         recoveryLinks.clear();
-        recoveryLinkRepository.findAll().forEach(recoveryLinks::add);
+        recoveryLinks.addAll(recoveryLinkRepository.findAll());
         return IntStream.range(0, links.length)
-                .mapToObj(i ->
-                        Arguments.of(recoveryLinks.get(i).getUser(), recoveryLinks.get(i))
-                );
+                .mapToObj(i -> Arguments.of(recoveryLinks.get(i).getUser(), recoveryLinks.get(i)));
     }
-
-
 }
